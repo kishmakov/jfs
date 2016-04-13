4bff8d880acbb9d23a2bb35ffcd699fa3ed0a0db
Kirill Shmakov <shmakir@yandex.ru>
2016-04-13 08:24:28 +0300

    Jar compilation settled.

--- src/org/kshmakov/Main.java
+++ src/org/kshmakov/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("usage: program_name file_name");
+            System.out.println("usage: java -jar jfs.jar file_name");
             return;
         }
 
