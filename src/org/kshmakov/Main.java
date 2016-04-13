7a3e7b178373bee59489a8c651596df77571ec90
Kirill Shmakov <shmakir@yandex.ru>
2016-04-13 08:00:40 +0300

    Main class created.

---
+++ src/org/kshmakov/Main.java
@@ -0,0 +1,15 @@
+package org.kshmakov;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("usage: program_name file_name");
+            return;
+        }
+
+        for (String arg: args) {
+            System.out.println("argument: " + arg);
+        }
+    }
+}
